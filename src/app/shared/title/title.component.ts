0720e513c1b6ae4531cb65575944178900d2317a
Miguel <workingbeta01@gmail.com>
2024-02-24 19:16:57 -0500

    @Input required

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  selector: 'app-title',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './title.component.html',
-  styles: ``
+  template:`
+    <h1 class="text-3xl mb-5" > {{ title }} </h1>
+  `
 })
 export class TitleComponent {
+
+   @Input({ required: true }) title: string = '';
 
 }
