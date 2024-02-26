1a9dfbb350e3f7b81509d39ea844c59435300b18
Miguel <workingbeta01@gmail.com>
2024-02-26 18:24:59 -0500

    Triggers e interacciones

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '../../../shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views.component.html',
   styles: ``
 })
 export default class DeferViewsComponent {
+
+
 
 }
