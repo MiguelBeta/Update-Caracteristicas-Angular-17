0d657d9024e4596541d51145f9e0749773cb4f5f
Miguel <workingbeta01@gmail.com>
2024-02-27 13:03:01 -0500

    Transition an animation - view Transition

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true,
+        onViewTransitionCreated( transitionInfo ){
+          console.log({ transitionInfo });
+        },
+      }),
+      ),
     provideClientHydration(),
   ]
 };
